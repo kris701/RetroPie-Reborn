c81426e502de9d2596bfddb929eb89641567639a
Kristian Skov <kris701kj@gmail.com>
2023-09-07 21:17:29 +0200

    Initial implementation of the battery icon runner

--- Backround-Runner/Runners/BatteryIconRunner/__init__.py
+++ Backround-Runner/Runners/BatteryIconRunner/__init__.py
@@ -2,10 +2,48 @@ from enum import Enum
 
 from ..BaseIconRunner import BaseIconRunner as BIR
 from ...IconManager import IconManager as IM
-from .BatteryIcons import BatteryIcons as WI
+from .BatteryIcons import BatteryIcons as BI
 
 class BatteryIconRunner(BIR):
     IconName : str = "Battery"
+    _currentState : BI = BI.Battery10
+    
+    _chargeVoltage : float = 4.1
+    _ChargevoltageMap : dict = {
+            0: BI.Battery10,
+            1: BI.Battery20,
+            # ...
+        }
+    _voltageMap : dict = {
+            0: BI.Battery10,
+            1: BI.Battery20,
+            # ...
+        }
         
     def Update(self, iconManager : IM) -> None:
-        pass 
+        checkState : BI = self.GetBatteryState()
+        if checkState != self._currentState:
+            self._currentState = checkState
+            iconManager.RemoveIcon(self.IconName);
+            iconManager.AddIcon(checkState, self.IconName)        
+
+    def GetBatteryState(self) -> BI:
+        voltage = self.GetBatteryVoltage()
+        if voltage >= self._chargeVoltage:
+            state : BI = BI.Battery10
+            for v, s in self._ChargevoltageMap:
+                state = s
+                if voltage > v:
+                    break    
+        else:
+            state : BI = BI.Battery10
+            for v, s in self._voltageMap:
+                state = s
+                if voltage > v:
+                    break    
+                
+        return state
+
+    def GetBatteryVoltage(self) -> float:
+        # insert ADC implementation here
+        pass
