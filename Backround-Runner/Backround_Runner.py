02aba8d41707fd92d24664414c278c57358908a0
Kristian Skov Johansen <kris701kj@gmail.com>
2023-11-12 19:29:20 +0100

    Fix

--- Backround-Runner/Backround_Runner.py
+++ Backround-Runner/Backround_Runner.py
@@ -1,9 +1,9 @@
 import time
 
-import IconManager as IM
+from IconManager import IconManager as IM
 from Runners.WifiIconRunner import WifiIconRunner as WIR
 from Runners.BluetoothIconRunner import BluetoothIconRunner as BIR
-#from Runners.PerformanceIconRunner import PerformanceIconRunner as PIR
+from Runners.PerformanceIconRunner import PerformanceIconRunner as PIR
 from Runners.BatteryIconRunner import BatteryIconRunner as BaIR
 from Runners.FanIconRunner import FanIconRunner as FIR
 from Runners.UnderVoltageIconRunner import UnderVoltageIconRunner as UVIR
@@ -11,7 +11,7 @@ from Runners.UnderVoltageIconRunner import UnderVoltageIconRunner as UVIR
 iconManager : IM = IM()
 wifiIconRunner : WIR = WIR()
 bluetoothIconRunner : BIR = BIR()
-#performanceIconRunner : PIR = PIR()
+performanceIconRunner : PIR = PIR()
 batteryIconRunner : BaIR = BaIR()
 fanIconRunner : FIR = FIR()
 underVoltageIconRunner : UVIR = UVIR()
@@ -19,7 +19,7 @@ underVoltageIconRunner : UVIR = UVIR()
 while True:
     wifiIconRunner.Update(iconManager)
     bluetoothIconRunner.Update(iconManager)
-    #performanceIconRunner.Update(iconManager)
+    performanceIconRunner.Update(iconManager)
     batteryIconRunner.Update(iconManager)
     fanIconRunner.Update(iconManager)
     underVoltageIconRunner.Update(iconManager)
