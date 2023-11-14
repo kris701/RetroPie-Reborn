54bcca989805453467361ad819fee1bf90be13b1
Kristian Skov Johansen <kris701kj@gmail.com>
2023-11-14 19:43:13 +0100

    Fixed direction

--- Backround-Runner/Runners/PerformanceIconRunner/__init__.py
+++ Backround-Runner/Runners/PerformanceIconRunner/__init__.py
@@ -4,6 +4,16 @@ import re
 
 from .PerformanceIcons import PerformanceIcons as PI
 
+# 111100000000000001111
+# ||||             ||||_ under-voltage
+# ||||             |||_ currently throttled
+# ||||             ||_ arm frequency capped
+# ||||             |_ soft temperature reached
+# ||||_ under-voltage has occurred since last reboot
+# |||_ throttling has occurred since last reboot
+# ||_ arm frequency capped has occurred since last reboot
+# |_ soft temperature reached since last reboot
+
 class PerformanceIconRunner():
     IconName : str = "Perf"
     _envCmd="vcgencmd get_throttled"
@@ -22,9 +32,9 @@ class PerformanceIconRunner():
         try:
             throttled_output = subprocess.check_output(self._envCmd, shell=True, timeout=1)
             throttled_binary = bin(int(throttled_output.split(b'=')[1], 0)) 
-            if throttled_binary[1] == '1':
+            if throttled_binary[18] == '1':
                 state = PI.Capped
-            if throttled_binary[2] == '1':
+            if throttled_binary[19] == '1':
                 state = PI.Throttled
         except Exception:
             return state
--- Backround-Runner/Runners/UnderVoltageIconRunner/__init__.py
+++ Backround-Runner/Runners/UnderVoltageIconRunner/__init__.py
@@ -4,6 +4,16 @@ import re
 
 from .UnderVoltageIcons import UnderVoltageIcons as UI
 
+# 111100000000000001111
+# ||||             ||||_ under-voltage
+# ||||             |||_ currently throttled
+# ||||             ||_ arm frequency capped
+# ||||             |_ soft temperature reached
+# ||||_ under-voltage has occurred since last reboot
+# |||_ throttling has occurred since last reboot
+# ||_ arm frequency capped has occurred since last reboot
+# |_ soft temperature reached since last reboot
+
 class UnderVoltageIconRunner():
     IconName : str = "Undervolt"
     _envCmd="vcgencmd get_throttled" 
@@ -22,7 +32,7 @@ class UnderVoltageIconRunner():
         try:
             throttled_output = subprocess.check_output(self._envCmd, shell=True, timeout=1)
             throttled_binary = bin(int(throttled_output.split(b'=')[1], 0)) 
-            if throttled_binary[0] == '1':
+            if throttled_binary[20] == '1':
                 state = UI.UnderVoltage
         except Exception:
             return state
