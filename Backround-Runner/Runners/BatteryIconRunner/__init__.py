d3b46c1d24db6a160ba4b5c1962dd274ed8820b4
Kristian Skov Johansen <kris701kj@gmail.com>
2023-11-14 13:23:55 +0100

    average battery level

--- Backround-Runner/Runners/BatteryIconRunner/__init__.py
+++ Backround-Runner/Runners/BatteryIconRunner/__init__.py
@@ -1,6 +1,7 @@
 from enum import Enum
 import board
 import busio
+import statistics
 import adafruit_ads1x15.ads1015 as ADS
 from adafruit_ads1x15.analog_in import AnalogIn
 
@@ -12,21 +13,8 @@ class BatteryIconRunner():
     _i2c = busio.I2C(board.SCL, board.SDA)
     _ads = ADS.ADS1015(_i2c)
     _chan = AnalogIn(_ads, ADS.P0)
+    _avgList : list = []
     
-    _chargeVoltage : float = 4.1
-    _ChargevoltageMap : dict = {
-            2:   BI.BatteryAlert,
-            2.2: BI.Battery10,
-            2.4: BI.Battery20,
-            2.6: BI.Battery30,
-            2.8: BI.Battery40,
-            3:   BI.Battery50,
-            3.2: BI.Battery60,
-            3.4: BI.Battery70,
-            3.6: BI.Battery80,
-            3.8: BI.Battery90,
-            4:   BI.Battery100,
-        }
     _voltageMap : dict = {
             2:   BI.BatteryAlert,
             2.2: BI.Battery10,
@@ -50,21 +38,16 @@ class BatteryIconRunner():
 
     def GetBatteryState(self) -> BI:
         voltage = self.GetBatteryVoltage()
-        print(voltage)
-        if voltage >= self._chargeVoltage:
-            state : BI = BI.BatteryAlert
-            for v, s in self._ChargevoltageMap:
-                if voltage < v:
-                    break   
-                state = s
-        else:
-            state : BI = BI.BatteryAlert
-            for v in self._voltageMap:
-                if voltage < v:
-                    break    
-                state = self._voltageMap[v]
+        state : BI = BI.BatteryAlert
+        for v in self._voltageMap:
+            if voltage < v:
+                break    
+            state = self._voltageMap[v]
                 
         return state
 
     def GetBatteryVoltage(self) -> float:
-        return self._chan.voltage
+        self._avgList.append(self._chan.voltage)
+        if len(self._avgList) > 10:
+            self._avgList.pop()
+        return statistics.mean(self._avgList)
