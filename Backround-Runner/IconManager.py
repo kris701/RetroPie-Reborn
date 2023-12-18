c6b07d0957d2fc6ec0002f6d485bf644614bd394
Kristian Skov Johansen <kris701kj@gmail.com>
2023-12-18 15:54:15 +0100

    fixing issues in PNGView

--- Backround-Runner/IconManager.py
+++ Backround-Runner/IconManager.py
@@ -1,6 +1,8 @@
 import os
 import subprocess
 from enum import Enum
+import sys
+from PIL import Image
 
 class IconManager(object):
     PngviewPath : str = "/usr/local/bin/pngview"
@@ -10,39 +12,51 @@ class IconManager(object):
     Margin : int = 5
 
     _currents : dict = {}
-    _processes : dict = {}
+    _process
     _changed : bool = False
 
     def Update(self) -> None:
         if self._changed == False:
             return
         
-        for name in self._processes:
-            self._processes[name].terminate()
-            self._processes[name].wait();
-        self._processes = {}
-
-        counter : int = 1
+        self._process.terminate()
+        self._process.wait();
+        
+        images = []
         for name in self._currents:
-            offset = self.Resolution - counter * (self.Margin + self.IconOffset)
-            self._processes[name] = subprocess.Popen(self.PngviewCall + [str(offset), str(self._currents[name])])
-            counter += 1
+            images.append(Image.open(self._currents[name]))
+        widths, heights = zip(*(i.size for i in images))
+
+        total_width = sum(widths)
+        max_height = max(heights)
+
+        new_im = Image.new('RGB', (total_width, max_height))
+
+        x_offset = 0
+        for im in images:
+          new_im.paste(im, (x_offset,0))
+          x_offset += im.size[0]
+
+        new_im.save('temp.png')
+            
+        offset = self.Resolution - len(images) * (self.Margin + self.IconOffset)
+        self._process = subprocess.Popen(self.PngviewCall + [str(offset), str("temp.png")])
 
         self._changed = False
     
     def AddIcon(self, icon, name : str) -> None:
         if str(icon) is "Hide":
             self.RemoveIcon(name)
-        elif name not in self._processes:
+        elif name not in self._currents:
             if os.path.exists(str(icon)):
                 self._currents[name] = icon
                 self._changed = True
         
     def RemoveIcon(self, name : str) -> None:
-        if name in self._processes:
-            self._processes.pop(name)
+        if name in self._currents:
+            self._currents.pop(name)
             self._changed = True
             
     def ClearIcons(self) -> None:
-        for name in self._processes:
+        for name in self._currents:
             self.RemoveIcon(name);
